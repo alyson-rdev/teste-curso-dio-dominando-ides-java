d07c7ff7c448038de4f0718a7a395c4e56e9b56d
alyson-rdev <83791574+alyson-rdev@users.noreply.github.com>
2021-07-23 17:00:39 -0300

    First Commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,28 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+      public static void main(String[] args) {
+    	  
+    	  Gato gato = new Gato();
+    	  
+    	  System.out.println(gato);
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  /*int a = 2;
+    	  int b = 3;
+    	  int c = a+b;
+		System.out.println("Hello World! " + "O resultado da soma de " + (a) + " + " + (b) + " é igual à; " + (c));*/
+		
+	}
+
+}
